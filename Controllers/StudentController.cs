4b275b20305f62441e51afd93198d6d0ce4781dd
salman <hashmisalman7040@gmail.com>
2024-12-14 00:29:04 +0530

    full CRUD Operation Complete

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -2,6 +2,7 @@
 using CRUD_Opration.Models;
 using CRUD_Opration.Models.Entity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CRUD_Opration.Controllers
 {
@@ -20,6 +21,8 @@ namespace CRUD_Opration.Controllers
             return View();
         }
 
+
+        //saving data to the sql database 
         [HttpPost]
         public  async Task<IActionResult> Add(AddStudentViewModel viewModel )
         {
@@ -34,6 +37,47 @@ namespace CRUD_Opration.Controllers
             await dbContext.Students.AddAsync(student);
             await dbContext.SaveChangesAsync();
             return View();
+        }
+
+        [HttpGet]
+        public  async Task<IActionResult> list()
+        {
+            var student = await dbContext.Students.ToListAsync();
+            return View(student);
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid id) {
+            var student = await dbContext.Students.FindAsync(id);
+            return View(student);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Student viewModel)
+        {
+            var student = await dbContext.Students.FindAsync(viewModel.Id);
+            if(student is not null )
+            {
+            student.Name = viewModel.Name;
+            student.Email = viewModel.Email;
+            student.Phone = viewModel.Phone;
+            student.Subscribe = viewModel.Subscribe;
+
+             await dbContext.SaveChangesAsync();
+            }
+            return RedirectToAction("list", "Student");
+        }
+        [HttpPost]
+        public async Task<IActionResult> Delete(Student viewModel)
+        {
+            var student = await dbContext.Students.FindAsync(viewModel.Id);
+            if (student is not null)
+            {
+                dbContext.Remove(student);
+                await dbContext.SaveChangesAsync();
+            }
+            return RedirectToAction("list", "Student");
         }
     }
 }
